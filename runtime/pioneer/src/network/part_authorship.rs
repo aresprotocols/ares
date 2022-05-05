bb241fd25b2f952104e23a693ed3b8512566aabb
zzm <zzmwork8@gmail.com>
2022-05-05 18:21:04 +0800

    modify pallet_authorship::Config EventHandler

--- runtime/pioneer/src/network/part_authorship.rs
+++ runtime/pioneer/src/network/part_authorship.rs
@@ -9,6 +9,6 @@ impl pallet_authorship::Config for Runtime {
 	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Babe>;
 	type UncleGenerations = UncleGenerations;
 	type FilterUncle = ();
-	type EventHandler = (Staking, ImOnline);
+	type EventHandler = (Staking, ImOnline, AresOracle);
 	// type EventHandler = Staking;
 }
